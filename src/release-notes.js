db3e65099fc304735d7a371d97f240f007b4f29a
Jason Z <jason.zissman@gmail.com>
2019-12-14 19:49:30 -0600

    Prettifying fade out

--- src/release-notes.js
+++ src/release-notes.js
@@ -17,9 +17,16 @@ let html = `
     position: absolute;
     top: 0px;
     left: 0px;
+    visibility: visible;
 }
 #release-notes-container {
-    font-family: Verdana, Geneva, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    font-family: Roboto,RobotoDraft,Helvetica,Arial,sans-serif;
+    font-size: .875rem;
+    letter-spacing: .2px;
+    color: #5f6368;
+    padding: 0 24px;
+    line-height: 20px;    
     position: absolute;
     z-index: 9999;
     width: 90%;
@@ -30,13 +37,34 @@ let html = `
     left: 50%;
     transform: translate(-50%,-50%);
     top: 50%;
-    border-radius: 10px;
-    padding: 15px;
+    border-radius: 6px;
+    padding: 24px;
+    background: rgba(256,256,256,1);
+}
+#cancel-path {
+    stroke: #5f6368;
+}
+#release-notes-overlay.gone, #release-notes-container.gone {
+    background: rgba(0,0,0,0);
+    color: #FFFFFF;
+    visibility: hidden;    
+    -webkit-transition: visibility .4s, background 0.4s linear, color 0.3s linear;
+    -moz-transition:    visibility .4s, background 0.4s linear, color 0.3s linear;
+    -ms-transition:     visibility .4s, background 0.4s linear, color 0.3s linear;
+    -o-transition:      visibility .4s, background 0.4s linear, color 0.3s linear;
+    transition:         visibility .4s, background 0.4s linear, color 0.3s linear;
+}
+#release-notes-container.gone #cancel-path {
+    stroke: #FFFFFF;
+    -webkit-transition: stroke 0.3s linear;
+    -moz-transition:    stroke 0.3s linear;
+    -ms-transition:     stroke 0.3s linear;
+    -o-transition:      stroke 0.3s linear;
+    transition:         stroke 0.3s linear;
 }
 .release-notes-header {
     margin: 0px 0px 0px 0px;
-    padding-bottom: 5px;
-    border-bottom: 0.5px solid #cccccc;
+    padding-bottom: 5px;    
 }
 .release-notes-header h1 {
     margin: 0px;
@@ -44,8 +72,8 @@ let html = `
 }
 #release-notes-close {
     position: absolute;
-    top: 10px;
-    right: 10px;
+    top: 15px;
+    right: 15px;
     text-align: center;
     border-radius: 50%;
     display: block;
@@ -60,16 +88,14 @@ let html = `
 }
 #release-notes-close:hover {
     background-color: #f8f8f8;
-
 }
 #release-notes-close span {
     position: absolute;
-    top: 5px;
-    right: 10px;
+    top: 4px;
+    right: 4px;
     font-weight: bold;
 }
-.release-notes-content {
-    font-size: 0.9rem;
+.release-notes-content {    
     overflow-y: auto;
     max-height: 330px;
 }
@@ -80,7 +106,12 @@ let html = `
     <header class="release-notes-header">
         <h1>December 2019 Update</h1>
         <span id="release-notes-close">
-            <span>X</span>
+            <span>
+            <svg width="24px" height="24px">
+                <path id="cancel-path" d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z">
+                </path>
+            </svg>
+            </span>
         </span>
     </header>
     <section>
@@ -106,8 +137,8 @@ let html = `
 </div>
 `
 let closeDialog = () => {
-    document.getElementById('release-notes-overlay').style.display = 'none';
-    document.getElementById('release-notes-container').style.display = 'none';
+    document.getElementById('release-notes-overlay').classList.add("gone");
+    document.getElementById('release-notes-container').classList.add("gone");
 };
 let loadReleaseNotes = () => {
     document.body.insertAdjacentHTML('beforeend', html);
